44d2120aabbb3f8c8e0f0586c1aaf01ec7fe2ba2
KarolusPaulus <karosaarinenn@gmail.com>
2024-03-29 13:24:42 +0200

    update

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -7,4 +7,77 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/:usern',function(request,response){
+  account.getAccount(request.params.usern, function(err,result){
+      if(err){
+          response.send(err);
+      }
+      else{
+          console.log(result);
+          response.json(result[0]);
+      }
+  })
+});
+
+router.get('/:usern',function(request,response){
+  account.selectAccount(request.params.usern, function(err,result){
+      if(err){
+          response.send(err);
+      }
+      else{
+          console.log(result);
+          response.json(result[0]);
+      }
+  })
+});
+
+router.get('/:usern',function(request,response){
+  account.getBalance(request.params.usern, function(err,result){
+      if(err){
+          response.send(err);
+      }
+      else{
+          console.log(result);
+          response.json(result[0]);
+      }
+  })
+});
+
+router.get('/:usern',function(request,response){
+  account.getLimit(request.params.usern, function(err,result){
+      if(err){
+          response.send(err);
+      }
+      else{
+          console.log(result);
+          response.json(result[0]);
+      }
+  })
+});
+
+router.delete('/:usern',function(request, response){
+  account.deleteAccount(request.params.usern, function(err, result){
+      if(err){
+          response.send(err);
+      }
+      else{
+          //response.json(result);
+          console.log(result.affectedRows);
+          response.json(result.affectedRows);
+      }
+  });
+});
+
+router.post('/',function(request, response){
+  account.addAccount(request.body, function(err, result){
+      if(err){
+          response.send(err);
+      }
+      else{
+          response.json(result);
+          //response.send(result[0].affectedRows)
+      }
+  });
+});
+
 module.exports = router;
--- backend/routes/card.js
+++ backend/routes/card.js
@@ -7,4 +7,43 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/',function(request, response){
+  if(request.body.username && request.body.password){
+      card.login(request.body.username, function(err,result){
+          if(err){
+              console.log(err.errno);
+              response.json(err.errno);
+          }
+          else{
+              if(result.length >0){
+                  bcrypt.compare(request.body.password, result[0].password, function(err, compareResult){
+                      if(compareResult){
+                          console.log('Kirjautuminen ok');
+                          const token=genToken({username: request.body.username});
+                          response.send(token);
+                      }
+                      else {
+                          console.log("Väärä salasana");
+                          response.send(false);
+                      }
+                  })
+              }
+              else {
+                  console.log("tunnusta ei ole");
+                  response.send(false);
+              }
+
+          }
+      });
+  }
+  else {
+      console.log("Tunnus tai salasana puuttuu")
+      response.send(false);
+  }
+});
+
+function genToken(value){
+  return jwt.sign(value, process.env.MY_TOKEN, {expiresIn: '200s'});
+}
+
 module.exports = router;
