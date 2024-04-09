2973ae430762899f38bfec86bb3651cc9b5b5ac2
KarolusPaulus <karosaarinenn@gmail.com>
2024-04-09 11:29:11 +0300

    update

--- backend/routes/view_transactions.js
+++ backend/routes/view_transactions.js
@@ -3,42 +3,27 @@ var router = express.Router();
 var transaction = require('../models/transaction_model');
 
 
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-router.get('/:viewtransactions', function(request, response) {
-    if (!request.query.start || !request.query.end || isNaN(request.query.start) || isNaN(request.query.end)) {
-      response.send("Invalid parameters");
+router.get('/', function(request, response) {
+    /*if (!request.body.start || !request.body.end || !request.body.idaccount || isNaN(request.body.start) || isNaN(request.body.end) || isNaN(request.body.idaccount)) {
+      response.send('Invalid parameters');
       return;
-    }
+    }*/
 
-    function getTransactionHistoryLoop(viewtransactions, start, end, callback) {
-      transaction.getTransactionHistoryInRange(viewtransactions, start, end, function(err, result) {
+      transaction.getTransactionHistoryInRange(request.body.idaccount, request.body.start, request.body.end, function(err, result) {
           if (err) {
               callback(err);
           } else {
               console.log(result);
-              response.json(result);
 
               if (result.length > 0) {
-                  const lastTransactionDate = result[result.length - 1].date;
-                  getTransactionHistoryLoop(viewtransactions, start, lastTransactionDate, callback);
+                  let lastTransactionDate = result[result.length - 1].date;
+                  console.log(lastTransactionDate);
+                  response.send(result);
               } else {
-                  callback(null, "All transactions fetched");
+                  callback(null, 'All transactions fetched');
               }
           }
       });
-  }
-
-  getTransactionHistoryLoop(request.params.viewtransactions, request.query.start, request.query.end, function(err, result) {
-    if (err) {
-      response.send(result);
-    } else {
-      console.log(result);
-      response.send(result);
-    }
-});
 });
 
 
